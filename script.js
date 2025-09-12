a2014b3234a2d838aca0308a5d3e9b046c0ef2b2
Guarnetti <136714642+Toxin4@users.noreply.github.com>
2025-09-11 22:04:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function iniciarMapa() {
             lat: -22.2349,
             lng: -49.9709
         },
-        zoom: 14
+        zoom: 14,
+        mapId: "teste"
     });
 
     directionsService = new google.maps.DirectionsService();
@@ -26,6 +27,7 @@ function iniciarMapa() {
     });
 
     definirEventos();
+    definirSearchBox();
 }
 
 function definirEventos() {
@@ -72,6 +74,58 @@ function adicionarPontoRota(posicao) {
         } else {
             console.error(s, r);
         }
-    })
+    });
+}
 
+let searchMarker = null;
+
+function definirSearchBox() {
+    //pega o input de pesquisa
+    var search = document.getElementById("search");
+
+    var searchBox = new google.maps.places.Autocomplete(search);
+
+    // fala para o google pesquisar nos limites do mapa (proximo daonde está o mapa)
+    searchBox.bindTo("bounds", map);
+
+    searchBox.addListener("place_changed", () => {
+        const place = searchBox.getPlace();
+        if (!place.geometry){
+            console.log("local não encontrado");
+            return;
+        }
+
+        map.setCenter(place.geometry.location);
+        map.setZoom(16);
+        
+        if (searchMarker) {
+            searchMarker.position = place.geometry.location;
+        } else {
+            var markerContent = document.createElement("div")
+            markerContent.style.backgroundColor = "red";
+            markerContent.style.color = "white";
+            markerContent.style.borderRadius = "50%";
+            markerContent.style.height = "50px";
+            markerContent.style.width = "50px";
+            markerContent.style.fontSize = "16px";
+            markerContent.style.fontWeight = "bold";
+            markerContent.style.display = "flex";
+            markerContent.style.alignContent = "center";
+
+            markerContent.innerHTML = "PESQUISA";
+            markerContent.classList.add("marker");
+
+
+            searchMarker = new google.maps.marker.AdvancedMarkerElement({
+                map: map,
+                position: place.geometry.location,
+                content: markerContent
+            });
+
+            // toda vez que clica na bolinha, muda a cor para uma aleatoria
+            searchMarker.addListener("click", (event) => {
+                markerContent.style.backgroundColor = "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, "0");
+            })
+        }
+    });
 }
